d31510bf20449be3eddc5021cb4ffa5f6b96abac
BranetE <elatedexorcist@gmail.com>
2024-03-19 17:09:54 +0200

    Added roles for user and statuses for order

---
+++ src/main/java/org/example/carpartsshop/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package org.example.carpartsshop.model;
+
+public enum OrderStatus {
+    CREATED, CONFIRMED, FINISHED, CANCELED
+}
---
+++ src/main/java/org/example/carpartsshop/model/UserRole.java
@@ -0,0 +1,5 @@
+package org.example.carpartsshop.model;
+
+public enum UserRole {
+    ADMIN, USER
+}
